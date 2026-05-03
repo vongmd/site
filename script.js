f75aad5f337440df2220ccfe84a0c89e675bdb90
vongmd <vicente.duarte@gmail.com>
2026-05-03 18:06:40 +0100

    feat: integração de tooltips dinâmicos com mapeamento de eventos

--- script.js
+++ script.js
@@ -34,16 +34,35 @@ d3.csv("data/dados.csv").then(dados => {
   svg.append("g")
     .call(d3.axisLeft(y));
 
-  // --- Renderização das Barras com Atribuição de Classe ---
+  // Seleção do elemento de tooltip
+  const tooltip = d3.select("#tooltip");
+
+  // --- Renderização das Barras com Eventos de Interação ---
   svg.selectAll("rect")
     .data(dados)
     .enter()
     .append("rect")
-      .attr("class", "bar") // Atribui a classe definida no style.css
+      .attr("class", "bar")
       .attr("x", d => x(d.categoria))
       .attr("y", d => y(d.valor))
       .attr("width", x.bandwidth())
-      .attr("height", d => alturaConteudo - y(d.valor));
+      .attr("height", d => alturaConteudo - y(d.valor))
+      // Evento: O rato entra na barra
+      .on("mouseover", (event, d) => {
+        tooltip
+          .style("opacity", 1)
+          .html(`Categoria: ${d.categoria}<br>Valor: ${d.valor}`);
+      })
+      // Evento: O rato move-se dentro da barra (ajusta a posição da etiqueta)
+      .on("mousemove", (event) => {
+        tooltip
+          .style("left", (event.pageX + 10) + "px")
+          .style("top", (event.pageY - 10) + "px");
+      })
+      // Evento: O rato sai da barra
+      .on("mouseout", () => {
+        tooltip.style("opacity", 0);
+      });
 
 }).catch(erro => {
   console.error("Erro no processamento de dados:", erro);
